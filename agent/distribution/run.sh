1deee93c2abe3b9542f855bf311e82e3cdbf4af1
Mariame Esther BARRY <Maresther@MacBook-Pro.local>
2024-05-26 17:59:04 +0200

    First basic rfswarm-agent docker + remove empty repository

--- agent/distribution/run.sh
+++
@@ -1,4 +0,0 @@
-!/bin/bash
-# This command launch the agent container
-# python /usr/local/lib/python3.12/site-packages/rfswarm_agent/rfswarm_agent.py  -i /src/$AGENT_INI_FILE  -r $ROBOT_EXE_LOCAL_PATH  -g $AGENT_LOG_LEVEL
-rfswarm-agent  -i /src/$AGENT_INI_FILE  -r $ROBOT_EXE_LOCAL_PATH  -g $AGENT_LOG_LEVEL
