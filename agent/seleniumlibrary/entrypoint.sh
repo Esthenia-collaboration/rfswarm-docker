ccdf8ac18681a0519dd71852bbf2233f71f201e5
Esther <esther.barry.lbs@gmail.com>
2024-08-13 12:53:14 +0200

    Rename images repositories in check_build_push_template file

--- agent/seleniumlibrary/entrypoint.sh
+++ agent/seleniumlibrary/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# This file launched rfswarm agent with default or user arguments
 DEFAULT_ARGS="-m http://manager:8138 -g 2 -p container:docker"
 echo "Running rfswarm-agent..."
 # Use the default args and overwrite them with user arguments
