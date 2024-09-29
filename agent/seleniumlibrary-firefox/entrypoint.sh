edbe4ce9974164919be420ca98e81a10ae967d3b
Esther <esther.barry.lbs@gmail.com>
2024-09-29 16:43:08 +0200

    Generate firefox image

--- agent/seleniumlibrary-firefox/entrypoint.sh
+++ agent/seleniumlibrary-firefox/entrypoint.sh
@@ -5,4 +5,4 @@ GEOMETRY="${SCREEN_WIDTH}x${SCREEN_HEIGHT}x${SCREEN_DEPTH}"
 Xvfb :0 -screen 0 $GEOMETRY -nolisten tcp &
 echo "Test is launched"
 robot -d /tmp/reports/ /tmp/selenium_firefox_test.robot
-echo "Test launch completed"
+echo "Test completed"
