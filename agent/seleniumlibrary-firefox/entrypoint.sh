63075e75ee1ba01eb878b1f557bed8031db1868f
KseniaYa <58310972+KseniaYa@users.noreply.github.com>
2024-09-30 18:27:39 +0200

    change error code if test failed

--- agent/seleniumlibrary-firefox/entrypoint.sh
+++ agent/seleniumlibrary-firefox/entrypoint.sh
@@ -4,5 +4,5 @@ rm -rf  /tmp/.X0-lock  # To remove the lock in case where the GUI server is alre
 GEOMETRY="${SCREEN_WIDTH}x${SCREEN_HEIGHT}x${SCREEN_DEPTH}"
 Xvfb :0 -screen 0 $GEOMETRY -nolisten tcp &
 echo "Test is launched"
-robot -d /tmp/reports/ /tmp/selenium_firefox_test.robot
+robot -d /tmp/reports/ /tmp/selenium_firefox_test.robot || exit 1
 echo "Test completed"
