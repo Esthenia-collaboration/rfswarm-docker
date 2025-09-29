289f42b711261e5b335ef80813d2b09ef8c5095e
Maresther-B <esther.barry.lbs@gmail.com>
2025-09-29 16:27:22 +0200

    add user non root user+ base image previous build

--- manager/entrypoint.sh
+++ manager/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 echo 'Manager Started'
 echo 'Running scenario.rfs file...'
-DEFAULT_ARGS="-i /src/manager/manager.ini -s /src/manager/scenario.rfs -g  3  -n"
-python   /root/.local/lib/python3.11/site-packages/rfswarm_manager/rfswarm.py $DEFAULT_ARGS "$@"
+#DEFAULT_ARGS="-i /src/manager/manager.ini -s /src/manager/scenario.rfs -g  3  -n"
+#python   /root/.local/lib/python3.13/site-packages/rfswarm_manager/rfswarm.py $DEFAULT_ARGS "$@"
+DEFAULT_ARGS="-i /src/manager/manager.ini -g  3  -n"
+python   /root/.local/lib/python3.13/site-packages/rfswarm_manager/rfswarm.py $DEFAULT_ARGS "$@"
 echo 'Manager down correctly'
