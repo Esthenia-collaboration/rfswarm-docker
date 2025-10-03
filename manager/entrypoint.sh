4ace1ef2fd7229bc98f16c53763a93a75946a719
Maresther-B <esther.barry.lbs@gmail.com>
2025-10-03 17:25:39 +0200

    taking in account Ksenia PR comment

--- manager/entrypoint.sh
+++ manager/entrypoint.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env sh
 echo 'Manager Started'
 echo 'Running scenario.rfs file...'
-#DEFAULT_ARGS="-i /src/manager/manager.ini -s /src/manager/scenario.rfs -g  3  -n"
-#python   /root/.local/lib/python3.13/site-packages/rfswarm_manager/rfswarm.py $DEFAULT_ARGS "$@"
 DEFAULT_ARGS="-i /src/manager/manager.ini -g  3  -n"
 python   /root/.local/lib/python3.13/site-packages/rfswarm_manager/rfswarm.py $DEFAULT_ARGS "$@"
 echo 'Manager down correctly'
