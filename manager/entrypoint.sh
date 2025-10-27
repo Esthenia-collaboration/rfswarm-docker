01038c1c4d8fd260b88b827004dbc4f83cc6c6fb
KseniaYa <58310972+KseniaYa@users.noreply.github.com>
2025-10-27 14:16:42 +0100

    feat: upload artifacts after tests execution (#89)
    Co-authored-by: ksenia.yankovich <k.yankovich@axione.fr>

--- manager/entrypoint.sh
+++ manager/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 echo 'Manager Started'
 echo 'Running scenario.rfs file...'
-DEFAULT_ARGS="-i /src/manager/manager.ini -g  3  -n"
+DEFAULT_ARGS="-i /src/manager/manager.ini -g 3 -n"
 python   /root/.local/lib/python3.13/site-packages/rfswarm_manager/rfswarm.py $DEFAULT_ARGS "$@"
 echo 'Manager down correctly'
