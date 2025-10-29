02ca169ec583ae480698fd6f013f5b95c98e8f03
Xixou971 <xixou971@gmail.com>
2025-10-29 06:46:20 +0100

    manager enhancement

--- manager/entrypoint.sh
+++ manager/entrypoint.sh
@@ -1,6 +1,34 @@
 #!/usr/bin/env sh
+# set -x
+
+ARGS="$@"
+
+# default values
+CONFIG_FILE=${CONFIG_FILE:-/src/manager/manager.ini}
+SCENARIO_FILE=${SCENARIO_FILE:-/src/manager/scenario.rfs}
+PORT=${PORT:-8138}
+LOG_LEVEL=${LOG_LEVEL:-3}
+DISPLAY_GUI=${DISPLAY_GUI:-false}
+RUN_AUTOMATICALLY=${RUN_AUTOMATICALLY:-false}
+
+# convert DISPLAY_GUI to lower case
+DISPLAY_GUI_LOWER=$(echo "$DISPLAY_GUI" | tr '[:upper:]' '[:lower:]')
+if [ "$DISPLAY_GUI_LOWER" = "true" ] || [ "$DISPLAY_GUI_LOWER" = "yes" ] || [ "$DISPLAY_GUI_LOWER" = "1" ]; then
+    echo "GUI activated"
+else
+    ARGS="--nogui $ARGS"
+fi
+
+# convert RUN_AUTOMATICALLY to lower case
+RUN_AUTOMATICALLY_LOWER=$(echo "$RUN_AUTOMATICALLY" | tr '[:upper:]' '[:lower:]')
+if [ "$RUN_AUTOMATICALLY_LOWER" = "true" ] || [ "$RUN_AUTOMATICALLY_LOWER" = "yes" ] || [ "$RUN_AUTOMATICALLY_LOWER" = "1" ]; then
+    echo "Run the scenario automatically after loading"
+    ARGS="--run $ARGS"
+fi
+
 echo 'Manager Started'
-echo 'Running scenario.rfs file...'
-DEFAULT_ARGS="-i /src/manager/manager.ini -g  3  -n"
-python   /root/.local/lib/python3.13/site-packages/rfswarm_manager/rfswarm.py $DEFAULT_ARGS "$@"
+COMMAND="python rfswarm.py --port $PORT --ini $CONFIG_FILE --scenario $SCENARIO_FILE --debug $LOG_LEVEL $ARGS"
+echo "Run commande ==> $COMMAND"
+exec $COMMAND
 echo 'Manager down correctly'
+ 
