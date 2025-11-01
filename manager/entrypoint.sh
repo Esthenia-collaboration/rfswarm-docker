6f0a28efefc00d08d3fb503bc708b812def5c0b6
Maresther-B <esther.barry.lbs@gmail.com>
2025-11-01 17:38:38 +0100

    taking in account review comments

--- manager/entrypoint.sh
+++ manager/entrypoint.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env sh
-echo 'Init secured ssh container...'
-#check sshd folder existance
-mkdir -p /var/run/sshd
-#run sshd in daemon mode
-echo 'Start ssh server'
-exec /usr/sbin/sshd -D  &
-
 echo 'Manager Started'
 echo 'Running scenario.rfs file...'
 DEFAULT_ARGS="-i /src/manager/manager.ini -g 3 -n"
